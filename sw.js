9e9a2af0eba044c660c80d2a298e5f71d595221a
C1pherFlow <llw884531170@qq.com>
2025-11-11 17:34:59 +0800

    Site updated: 2025-11-11 17:34:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761455812754";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1762853688853";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
